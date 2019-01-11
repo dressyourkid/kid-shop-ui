4b7d4e4b0d99002c018c651cdc343f567a4362b6
luckykeks <luckykeks@gmail.com>
2019-01-11 22:23:11 +0300

    page-login few fix for login form

--- pages/login/Login.ts
+++ pages/login/Login.ts
@@ -1,18 +1,6 @@
+import { Component,Vue } from "nuxt-property-decorator"
 
-  export default {
-    data: () => ({
-      valid: false,
-      firstname: '',
-      lastname: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters'
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid'
-      ]
-    })
-  }
-  
+@Component({})
+
+export default class ProductById extends Vue {
+}
---
+++ pages/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+   <v-form>
+      <v-container>
+         <v-layout 
+            row
+            justify-center
+         >
+            <v-flex
+               xs12
+               md4
+               lg4
+            >
+               <v-text-field label="Name">
+               </v-text-field>
+               <v-text-field label="Password">
+               </v-text-field>
+               <v-btn color="blue">
+                  VK
+               </v-btn>
+               <v-btn color="blue">
+                  FACEBOOk
+               </v-btn>
+               <v-btn color="green">
+                  ENTER
+               </v-btn>
+            </v-flex>
+         </v-layout>
+      </v-container>
+   </v-form>
+</template>
+
+<script lang="ts" src="./Login.ts">
+</script>   
