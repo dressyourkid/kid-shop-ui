0cecf37810a078819f8026a498427f685b360f6a
luckykeks <luckykeks@gmail.com>
2019-01-11 20:49:28 +0300

    singke-product initial comment

---
+++ pages/products/_id.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h1 class="header">Single page test</h1>
+    <v-btn>ZAPUSKAI SUKA</v-btn>
+    <v-btn>ZAPUSKAI SUKA</v-btn>
+  </section>
+</template>
+
+<script lang="ts" src="./ProductById.ts">
+</script>
