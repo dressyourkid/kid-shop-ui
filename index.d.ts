37af06f44d464cd54eaac40d4a9aae55c94149cc
lconnected <levas1992@gmail.com>
2019-01-14 15:09:25 +0300

    friendly-errors fixed

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { Route, RawLocation, NavigationGuard } from "vue-router";
+import '@nuxtjs/axios';
+// import { AxiosInstance, AxiosRequestConfig } from 'axios'
 
 declare module '*.vue' {
   import Vue from 'vue'
