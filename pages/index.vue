b24851502075f2a92dfbe0126908c0b32d27cd18
luckykeks <luckykeks@gmail.com>
2019-01-15 16:33:17 +0300

    fix indents and few issues in ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,8 @@
 <template>
-  <section>
-    <h1 class="header">Nuxt TypeScript Starter</h1>
-    <v-btn>Vuetify button</v-btn>
-    <div>
-        
-    </div>
-    <div class="cards">
-      <Card
-        v-for="person in people"
-        :key="person.id"
-        :person="person"
-      ></Card>
-    </div>
-  </section>
+    <section>
+        <p>Определится с тем что будет показываться на главной странице</p>  
+    </section>
 </template>
 
-<script lang="ts">
-import {
-  Component,
-  Vue
-} from "nuxt-property-decorator"
-import { State } from "vuex-class"
-import { Person } from "~/types";
-import Card from "~/components/Card.vue"
-
-@Component({
-  components: {
-    Card
-  }
-})
-export default class extends Vue {
-  @State people: Person
-}
+<script lang="ts" src="./index.ts">
 </script>
-
-<style scoped>
-.header {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-}
-
-.cards {
-  display: flex;
-  flex-wrap: wrap;
-}
-</style>
--- pages/login/Login.ts
+++ pages/login/Login.ts
@@ -1,6 +1,5 @@
 import { Component,Vue } from "nuxt-property-decorator"
 
 @Component({})
-
 export default class ProductById extends Vue {
 }
