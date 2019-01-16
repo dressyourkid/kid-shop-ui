785cdade294e752d4bc8730b09bdc91b66606c93
luckykeks <luckykeks@gmail.com>
2019-01-16 13:05:17 +0300

    #3 added get request for single-product

--- pages/products/ProductById.ts
+++ pages/products/ProductById.ts
@@ -3,4 +3,10 @@ import { Component,Vue } from "nuxt-property-decorator"
 @Component({})
 
 export default class ProductById extends Vue {
+    private product: any[] = [];
+
+    private async mounted() {
+        const fetchProductById = await this.$axios.$get('product/' + this.$route.params.id);
+        this.product= fetchProductById;
+    }
 }
--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,9 +1,30 @@
 <template>
-  <section>
-    <h1 class="header">Single page test</h1>
-    <v-btn>ZAPUSKAI SUKA</v-btn>
-    <v-btn>ZAPUSKAI SUKA</v-btn>
-  </section>
+    <section>
+        <h1 class="header">Single page test</h1>
+        <v-layout row wrap>
+            <v-flex
+                xs12 
+                sm6
+                md4
+            >
+                <v-card class="ma-4">
+                    <v-img
+                        src="http://picture-cdn.wheretoget.it/6d8ng3-l-610x610-t+shirt-kids+tee-kids+fashion-kids+sweater-kids+swag-kid-graphic+tee-t+shirt+print-sweater-urban-streetwear-beanie-shirt-single-jacket-78-pull-black-swag-dope-white+t+shirt-flowers.jpg"
+                        aspect-ratio="1"
+                    ></v-img>
+                    <v-card-title primary-title>
+                    <div>
+                        <h3 class="headline mb-0">
+                            {{ product.name }}
+                        </h3>
+                        <div>{{ product.description }}</div>
+                        <div>PRICE: {{ product.price }}</div>
+                    </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </section>
 </template>
 
 <script lang="ts" src="./ProductById.ts">
