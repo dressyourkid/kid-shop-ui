1fdc430733bd062f3e00b2d41f8b12181cf8bfcc
lconnected <levas1992@gmail.com>
2019-01-17 01:27:34 +0300

    Fixed import issue with .vue files inside .ts filed

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,9 +2,3 @@ import Vue from 'vue'
 import VueRouter, { Route, RawLocation, NavigationGuard } from "vue-router";
 import '@nuxtjs/axios';
 // import { AxiosInstance, AxiosRequestConfig } from 'axios'
-
-declare module '*.vue' {
-  import Vue from 'vue'
-  const _default: Vue
-  export default _default
-}
