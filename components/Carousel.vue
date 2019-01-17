1b7cd0100287f263829e5b574b48c162f26030cd
luckykeks <luckykeks@gmail.com>
2019-01-17 19:24:16 +0300

    #9 carousel lvl2

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,16 +1,11 @@
 <template>
-    <v-carousel light hide-delimiters>
-        <v-carousel-item
+    <v-carousel hide-delimiters light>
+        <v-carousel-item 
             v-for="product in products"
-            :key="product.id"
-        >
-        <v-layout row wrap>
-            <v-flex
-                xs12 
-                sm6
-                md4
-            >
-                <v-card class="ma-4">
+            :key="product.id" >
+                <v-layout row>
+                    <v-flex xs4 :key="j" v-for="j in 5">
+                        <v-card class="ma-4">
                     <v-img
                         src="http://picture-cdn.wheretoget.it/6d8ng3-l-610x610-t+shirt-kids+tee-kids+fashion-kids+sweater-kids+swag-kid-graphic+tee-t+shirt+print-sweater-urban-streetwear-beanie-shirt-single-jacket-78-pull-black-swag-dope-white+t+shirt-flowers.jpg"
                         aspect-ratio="1"
@@ -43,11 +38,16 @@
                     </v-btn>
                     </v-card-actions>
                 </v-card>
-            </v-flex>
-        </v-layout>
+                    </v-flex>
+                </v-layout>
         </v-carousel-item>
     </v-carousel>
 </template>
 
 <script lang="ts" src="./Carousel.ts">
 </script>
+<style>
+img {
+  max-width: 100%;
+}
+</style>
