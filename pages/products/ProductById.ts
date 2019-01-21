fc7828acc16cbf581fd6ee74e5900c7d279b9da3
Denis Ivanov <alagorn21@mail.ru>
2019-01-21 19:19:21 +0300

    products id pages

--- pages/products/ProductById.ts
+++ pages/products/ProductById.ts
@@ -17,5 +17,14 @@ export default class ProductById extends Vue {
     private async created() {
         const fetchProductById = await this.$axios.$get('product/' + this.$route.params.id);
         this.product = fetchProductById;
-    }
+    };
+    private items: any[] = [
+        {
+            src: 'http://picture-cdn.wheretoget.it/6d8ng3-l-610x610-t+shirt-kids+tee-kids+fashion-kids+sweater-kids+swag-kid-graphic+tee-t+shirt+print-sweater-urban-streetwear-beanie-shirt-single-jacket-78-pull-black-swag-dope-white+t+shirt-flowers.jpg'
+          },
+          {
+            src: 'http://picture-cdn.wheretoget.it/6d8ng3-l-610x610-t+shirt-kids+tee-kids+fashion-kids+sweater-kids+swag-kid-graphic+tee-t+shirt+print-sweater-urban-streetwear-beanie-shirt-single-jacket-78-pull-black-swag-dope-white+t+shirt-flowers.jpg'
+          }
+	]
+
 }
--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,31 +1,79 @@
 <template>
     <section>
-        <h1 class="header">Single page test</h1>
-        <v-layout row wrap>
+        <h1 class="header text-xs-center">Single page test</h1>
+        <v-layout row wrap justify-center>
+            <v-flex xs1 sm1 md1></v-flex>
             <v-flex
-                xs12 
-                sm6
-                md4
+                xs12
+                sm7
+                md7
+                style="max-width: 600px;"
             >
-                <v-card class="ma-4">
-                    <v-img
-                        src="http://picture-cdn.wheretoget.it/6d8ng3-l-610x610-t+shirt-kids+tee-kids+fashion-kids+sweater-kids+swag-kid-graphic+tee-t+shirt+print-sweater-urban-streetwear-beanie-shirt-single-jacket-78-pull-black-swag-dope-white+t+shirt-flowers.jpg"
-                        aspect-ratio="1"
-                    ></v-img>
-                    <v-card-title primary-title>
-                    <div>
-                        <h3 class="headline mb-0">
-                            {{ product.name }}
-                        </h3>
-                        <div>{{ product.description }}</div>
-                        <div>PRICE: {{ product.price }}</div>
-                    </div>
-                    </v-card-title>
-                </v-card>
+                    <v-card class="ma-4">
+                        <v-carousel>
+                            <v-carousel-item
+                                v-for="(item,i) in items"
+                                :key="i"
+                                :src="item.src"
+                            ></v-carousel-item>
+                        </v-carousel>
+
+                        <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">
+                                {{ product.name }}
+                            </h3>
+                            <div>{{ product.description }}</div>
+                            <div>PRICE: {{ product.price }}</div>
+                        </div>
+                        </v-card-title>
+                    </v-card>
             </v-flex>
+            <v-flex                 
+                xs12 
+                sm3
+                md3
+                class="pl-3 pt-3 _id_product__info"
+            >
+                <v-navigation >
+                    <h1 class="headline"> {{product.name}} </h1>
+                    <p>яблоко сочное спелое </p>
+                    <v-flex 
+                        xs12 
+                        sm8 
+                        d-flex
+                    >
+                        <v-select
+                            :items="items"
+                            label="Выберите размер"
+                        >
+                        </v-select>
+                    </v-flex>
+                    <v-btn class="mt-5" large>Добавить в корзину</v-btn>
+                    <h3 class="mt-5">О товаре</h3>
+                    <p>{{ product.description }}</p>
+                    <div>
+                        <p>Цена: <span class="font-weight-bold"> {{ product.price }}</span></p>
+                    </div>
+                    <div>
+                        <p>В наличии: <span class="font-weight-bold"> {{ product.exists }}</span></p>
+                    </div>
+                    <div>
+                        <p>Страна производства: <span class="font-weight-bold"> aaa</span></p>
+                    </div>
+ 
+                </v-navigation>
+         </v-flex>
+         <v-flex xs1 sm1 md1></v-flex>
         </v-layout>
     </section>
 </template>
 
 <script lang="ts" src="./ProductById.ts">
 </script>
+
+<style>
+._id_product__info{
+    border-left: 1px solid black;
+}
+</style>
