a177f4a92f11ee279d155fdb6a080cc993de99c1
vladtitov7 <32096809+vladtitov7@users.noreply.github.com>
2019-01-23 01:23:41 +0300

    pagination: added simple pagination

--- pages/category/CategoryId.ts
+++ pages/category/CategoryId.ts
@@ -1,12 +1,38 @@
-import { Component,Vue } from "nuxt-property-decorator"
+import { Component, Vue, Watch } from "nuxt-property-decorator"
 
 @Component({
 })
 export default class CategoryId extends Vue {
     private categoryProducts: any[] = [];
+    private currentPage: number = 1;
+    private numberOfPages: number = null;
+    private pageSize: number = 9;
+
+    @Watch('currentPage')
+    private watchCurrentPage(val) {
+        this.currentPage = val;
+    //    this.$router.push(this.$route.path + '/page/' + this.currentPage);
+        this.getPageContent(this.currentPage);
+    }
     
     private async created() {
-        const categoryPage = await this.$axios.$get('category/' + this.$route.params.id + '/product');
-        this.categoryProducts = categoryPage.content;
+        this.getPageContent(this.currentPage);
+    }
+
+    private async getPageContent(page: number) {
+        try {
+            const pageNumber = (page - 1);
+            const params = {
+                page: pageNumber,
+                size: this.pageSize,
+            };
+            const categoryPage = await this.$axios.$get('category/' + this.$route.params.id + '/product', { params });
+            if (categoryPage) {
+                this.categoryProducts = categoryPage.content;
+                this.numberOfPages = categoryPage.totalPages;
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
