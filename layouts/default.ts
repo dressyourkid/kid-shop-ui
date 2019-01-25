ff5b754d65b501b88734af83de73a0e57f1171a5
luckykeks <luckykeks@gmail.com>
2019-01-25 23:53:27 +0300

    #25 addew search in toolbar

--- layouts/default.ts
+++ layouts/default.ts
@@ -7,12 +7,26 @@ import LoginModal from '~/components/login/LoginModal.vue';
 	}
 })
 export default class DefaultLayout extends Vue {
+	private productSearch: string = 'sidr';
 	private openDialog: boolean = false;
 	private categories: any[] = [];
 
 	private async mounted() {
         const categoriesPage = await this.$axios.$get('category');
         this.categories = categoriesPage.content;
-    }
+	}
+	
+	private async search() {
+		const result = await this.$axios.$get('/product', {
+			params: {
+				search: this.productSearch
+			}
+		})
+		.catch((error) => {
+			throw error;
+		});
+		
+		console.log(result);
+	}
 }
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,31 @@
     <v-app>
         <div>
             <v-toolbar class="yellow darken-3">
+                <v-layout align-center justify-space-between row>
                 <nuxt-link to="/">
                     <v-toolbar-title>
                         <v-icon>mdi-anchor</v-icon>
                         KidShop
                     </v-toolbar-title>
                 </nuxt-link>
-                <v-spacer></v-spacer>
+                <v-flex xs6>
+                    <v-text-field
+                        v-model="productSearch"
+                        label="Search" 
+                        single-line
+                        background-color="white"
+                        clearable
+                        append-icon="mdi-search-web"
+                        @keyup.enter="search"
+                    />
+                </v-flex>
                 <v-btn
                     outline      
                     @click="openDialog = true"
                 >
                     Sign in
                 </v-btn>
+                </v-layout>
             </v-toolbar>
             <v-layout row justify-center wrap> 
                 <div
