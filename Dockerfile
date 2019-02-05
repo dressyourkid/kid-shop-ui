63f61d6e1cc2839e4a6ae37850dff24cf05ca619
lconnected <levas1992@gmail.com>
2019-02-06 01:53:33 +0300

    deploy vol 2

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,9 @@ WORKDIR /usr/src/app
 
 COPY . /usr/src/app
 
+# RUN echo "--->>> Installing packages" && \
+#     npm install
+
 # RUN echo "--->>> Building app" && \
 #     npm run build
 
--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "building Dockerfile..."
-docker build -t dressyourkid/kid-shop-ui .
-echo "Dockerfile built successfully"
+# echo "building Dockerfile..."
+# docker build -t dressyourkid/kid-shop-ui .
+# echo "Dockerfile built successfully"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push dressyourkid/kid-shop-ui
